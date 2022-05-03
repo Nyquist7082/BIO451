1484d38cf5a2b7e391bf8a6bf06ddb7e444d64fd
Nyquist7082 <nils.nyquist7082@gmail.com>
2022-05-03 14:23:31 +0200

    data sheet loaded from google drive

--- BIO451_trial_script_main.R
+++ BIO451_trial_script_main.R
@@ -1,2 +1,5 @@
 # BIO451 2022 Group project
 # Fucus vesiculosus vs Littorina obtusata/fabalis trials
+
+library(googlesheets4)
+data <- read_sheet("https://docs.google.com/spreadsheets/d/1B8JgUlGrqr5kcPiw9d-mjmtuelmGU08LlfG2RV5sLBU/edit#gid=1501238862", range = "DummyData")
