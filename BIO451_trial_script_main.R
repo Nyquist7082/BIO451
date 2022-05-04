94b115e1191d77b45c394c256b4b9f0d1d1fe27d
Nyquist7082 <nils.nyquist7082@gmail.com>
2022-05-04 09:41:06 +0200

    #something

--- BIO451_trial_script_main.R
+++ BIO451_trial_script_main.R
@@ -3,3 +3,4 @@
 
 library(googlesheets4)
 data <- read_sheet("https://docs.google.com/spreadsheets/d/1B8JgUlGrqr5kcPiw9d-mjmtuelmGU08LlfG2RV5sLBU/edit#gid=1501238862", range = "DummyData")
+# something
