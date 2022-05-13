e0ad2da0bdec2db5f73ef41a794fdbc3330fbf02
Nyquist7082 <nils.nyquist7082@gmail.com>
2022-05-13 16:43:19 +0200

    cleaned up script

--- BIO451_data_load.R
+++ BIO451_data_load.R
@@ -3,13 +3,11 @@
 
 # Loading data
 
-library(vegan)
-library(ggplot2)
-library(reshape2)
-library(dplyr)
-library(ggpubr)        #Publication-ready graphics
-library(car)           #Regression
-library(corrplot)      #Correlation plots
+
 library(tidyverse)     #Data manipulation
-library(rstatix)       #Pipe-friendly R functions for easy statistical analyses
 library(googlesheets4) #Read google sheets data into R 
+
+trial1_data <- read_sheet("https://docs.google.com/spreadsheets/d/1B8JgUlGrqr5kcPiw9d-mjmtuelmGU08LlfG2RV5sLBU/edit#gid=1501238862", range = "trial1")
+
+preference_data <- read_sheet("https://docs.google.com/spreadsheets/d/1B8JgUlGrqr5kcPiw9d-mjmtuelmGU08LlfG2RV5sLBU/edit#gid=1501238862", range = "preference")
+
