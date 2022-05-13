33b754f6835f8523f287ba16f560ddb6f207b0fe
Nyquist7082 <nils.nyquist7082@gmail.com>
2022-05-13 16:51:42 +0200

    first commit preference trial grazing

---
+++ BIO451_preference_trial_grazing.R
@@ -0,0 +1,14 @@
+# BIO451 2022 Group project
+# Fucus vesiculosus vs Littorina obtusata/fabalis trials
+
+# Preference trial grazing
+
+library(vegan)
+library(ggplot2)
+library(dplyr)
+library(ggpubr)        #Publication-ready graphics
+library(car)           #Regression
+library(corrplot)
+library(tidyverse)     #Data manipulation
+library(rstatix)       #Pipe-friendly R functions for easy statistical analyses
+library(googlesheets4) #Read google sheets data into R 
