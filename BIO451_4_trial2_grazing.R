af9cbd48d3aa4fb6f5334ae9d2202c3b9d3c5044
jor949 <jor949@googlemail.com>
2022-05-16 16:36:25 +0200

    started some plotting in script 4 (permanova, ttest, boxplot, traits/grazing)

--- BIO451_4_trial2_grazing.R
+++ BIO451_4_trial2_grazing.R
@@ -12,3 +12,79 @@ library(corrplot)
 library(tidyverse)     #Data manipulation
 library(rstatix)       #Pipe-friendly R functions for easy statistical analyses
 library(googlesheets4) #Read google sheets data into R 
+
+# Add column: wetweight change in % ####
+preference_data$ww_perc <- (preference_data$`ww_after`- preference_data$`ww_before`)/(preference_data$`ww_before`)*100
+
+#boxplot ww_perc: exposed vs sheltered####
+p_ww_perc_sh_v_ex <- ggboxplot(preference_data, x = "ecotype", y = "ww_perc",
+                               color = "ecotype",
+                               add = "jitter", shape = "ecotype", width= 0.2)+
+  theme_classic()+
+  xlab("Environment")
+p_ww_perc_sh_v_ex + theme(legend.title = element_blank())
+
+
+#plots ww_perc vs STA ####
+
+p_ww_perc_sta <- ggplot(data = preference_data, 
+                        aes(x = STA,
+                            y = ww_perc,
+                            colour=ecotype)) + 
+  geom_smooth(method=lm) + 
+  geom_point(size = 2) +
+  theme_classic()
+p_ww_perc_sta + theme(legend.title = element_blank())
+
+#plots ww_perc vs SAP ####
+
+p_ww_perc_sap <- ggplot(data = preference_data, 
+                        aes(x = SAP,
+                            y = ww_perc,
+                            colour=ecotype)) + 
+  geom_smooth(method=lm) + 
+  geom_point(size = 2) +
+  theme_classic()
+p_ww_perc_sap + theme(legend.title = element_blank())
+
+#plots ww_perc vs TDMC####
+
+p_ww_perc_tdmc <- ggplot(data = preference_data, 
+                         aes(x = TDMC,
+                             y = ww_perc,
+                             colour=ecotype)) + 
+  geom_smooth(method=lm) + 
+  geom_point(size = 2) +
+  theme_classic()
+p_ww_perc_tdmc + theme(legend.title = element_blank())
+
+# PERMANOVA (non parametric)####
+
+preference.permanova <- adonis2(cbind(scale(preference_data$STA), scale(preference_data$SAP), 
+                                      scale(preference_data$TDMC)
+) ~ preference_data$ww_perc,
+permutations = 9999,
+method="euclidian")
+
+preference.permanova
+
+#STA ttest####
+
+ttest.STA <- preference_data %>% 
+  t_test(STA ~ ecotype) %>%
+  add_significance()
+ttest.STA
+
+#SAP ttest####
+
+ttest.SAP <- preference_data %>% 
+  t_test(SAP ~ ecotype) %>%
+  add_significance()
+ttest.SAP
+
+#TDMC ttest####
+
+ttest.TDMC <- preference_data %>% 
+  t_test(TDMC ~ ecotype) %>%
+  add_significance()
+ttest.TDMC
