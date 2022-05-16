2e190777bd10670ab7086f5011670ec342865b61
Nyquist7082 <nils.nyquist7082@gmail.com>
2022-05-16 16:27:28 +0200

    growth correction moved position

--- BIO451_1_data_load.R
+++ BIO451_1_data_load.R
@@ -15,16 +15,16 @@ trial1_data <- read_sheet("https://docs.google.com/spreadsheets/d/1B8JgUlGrqr5kc
 # Add column: wetweight change in % ####
 trial1_data$ww_perc <- (trial1_data$`ww_after`- trial1_data$`ww_before`)/(trial1_data$`ww_before`)*100
 
-# Grazed and baseline separation ####  
-trial1_data_ex_c <-trial1_data[trial1_data$treatment == 'grazed', ]   #Gazed, without baseline
-trial1_data_c <-trial1_data[trial1_data$treatment == 'not_grazed', ]  #Baseline
-
 # Growth corrected ####
 correct_growth <- trial1_data %>% select(sample_ID , ww_perc, treatment)
 correct_growth <- correct_growth %>% pivot_wider(names_from = "treatment", values_from = "ww_perc")
 correct_growth$growth_corr <- correct_growth$not_grazed- correct_growth$grazed
 trial1_data <- left_join(trial1_data, correct_growth, by = "sample_ID")
 trial1_data$growth_corr[trial1_data$treatment =="not_grazed"]=NA
+
+# Grazed and baseline separation ####  
+trial1_data_ex_c <-trial1_data[trial1_data$treatment == 'grazed', ]   #Gazed, without baseline
+trial1_data_c <-trial1_data[trial1_data$treatment == 'not_grazed', ]  #Baseline
 
 
 ###################### Trial 2: preference #####################################
