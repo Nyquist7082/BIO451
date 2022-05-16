1f9fa07a179b06059be343feec7b8c0575b7d321
Nyquist7082 <nils.nyquist7082@gmail.com>
2022-05-16 11:46:31 +0200

    boxplots

--- BIO451_3_trial1_grazing.R
+++ BIO451_3_trial1_grazing.R
@@ -6,9 +6,26 @@
 library(vegan)
 library(ggplot2)
 library(dplyr)
-library(ggpubr)        #Publication-ready graphics
-library(car)           #Regression
+library(ggpubr)        # Publication-ready graphics
+library(car)           # Regression
 library(corrplot)
-library(tidyverse)     #Data manipulation
-library(rstatix)       #Pipe-friendly R functions for easy statistical analyses
-library(googlesheets4) #Read google sheets data into R 
+library(tidyverse)     # Data manipulation
+library(rstatix)       # Pipe-friendly R functions for easy statistical analyses
+library(googlesheets4) # Read google sheets data into R 
+
+############################### Boxplots #######################################
+
+ggboxplot(trial1_data_ex_c, x = "ecotype", y = "ww_dif",
+          color = "ecotype", palette =c("#00AFBB", "#E7B800"),
+          add = "jitter", shape = "ecotype", title = "Grazed")
+
+ggboxplot(trial1_data_c, x = "ecotype", y = "ww_dif",
+          color = "ecotype", palette =c("#00AFBB", "#E7B800"),
+          add = "jitter", shape = "ecotype", title = "Baseline")
+
+ggboxplot(trial1_data, x = "ecotype", y = "ww_dif",
+          color = "treatment",
+          add = "jitter", shape = "treatment", title = "Both treatments")
+
+
+
