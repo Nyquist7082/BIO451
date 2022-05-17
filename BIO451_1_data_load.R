bfc902fa522ef7e37d7b85756baa6e51daf4f974
Nyquist7082 <nils.nyquist7082@gmail.com>
2022-05-17 09:50:14 +0200

    separation code readded

--- BIO451_1_data_load.R
+++ BIO451_1_data_load.R
@@ -15,7 +15,7 @@ trial1_data <- read_sheet("https://docs.google.com/spreadsheets/d/1B8JgUlGrqr5kc
 # Add column: wetweight change in % ####
 trial1_data$ww_perc <- (trial1_data$`ww_after`- trial1_data$`ww_before`)/(trial1_data$`ww_before`)*100
 
-#Calculations of dif. in PHL####
+# Calculations of dif. in PHL####
 trial1_data_wider <- trial1_data %>% pivot_wider(names_from = "treatment", values_from = "PHL")
 #view(trial1_data_wider)
 
@@ -35,6 +35,10 @@ correct_growth <- correct_growth %>% pivot_wider(names_from = "treatment", value
 correct_growth$growth_corr <- correct_growth$not_grazed- correct_growth$grazed
 trial1_data <- left_join(trial1_data, correct_growth, by = "sample_ID")
 trial1_data$growth_corr[trial1_data$treatment =="not_grazed"]=NA
+
+# Grazed and baseline separation #### 
+trial1_data_ex_c <-trial1_data[trial1_data$treatment == 'grazed', ]   #Gazed, without baseline
+trial1_data_c <-trial1_data[trial1_data$treatment == 'not_grazed', ]  #Baseline
 
 
 ###################### Trial 2: preference #####################################
