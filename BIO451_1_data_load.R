ca420226ec67333908bedaa8af65ba3a6d1afbef
hartiem <emma.j.hartikainen@gmail.com>
2022-05-17 10:03:26 +0200

    PHL try to correct

--- BIO451_1_data_load.R
+++ BIO451_1_data_load.R
@@ -15,18 +15,20 @@ trial1_data <- read_sheet("https://docs.google.com/spreadsheets/d/1B8JgUlGrqr5kc
 # Add column: wetweight change in % ####
 trial1_data$ww_perc <- (trial1_data$`ww_after`- trial1_data$`ww_before`)/(trial1_data$`ww_before`)*100
 
-# Calculations of dif. in PHL####
+#Calculations of dif. in PHL####
 trial1_data_wider <- trial1_data %>% pivot_wider(names_from = "treatment", values_from = "PHL")
-#view(trial1_data_wider)
+view(trial1_data_wider)
 
 select_1 <- trial1_data_wider %>% select(grazed,ecotype, sample_ID)
-select_1_removeNA <- na.omit(select_1)
+select_1_removeNA <- na.omit(try1)
 select_2 <- trial1_data_wider %>% select(not_grazed,ecotype,sample_ID)
-select_2_removeNA <- na.omit(select_2)
+select_2_removeNA <- na.omit(try2)
 joined <- full_join(select_1_removeNA, select_2_removeNA)
 joined$PHLdiff <- joined$grazed-joined$not_grazed
 #Just run it once!
-trial1_data <- left_join(trial1_data, joined, by="sample_ID")
+trial1_data <- left_join(trial1_data, joined)
+trial1_data$PHLdiff[trial1_data$treatment =="not_grazed"]=NA
+
 
 
 # Growth corrected ####
@@ -35,10 +37,6 @@ correct_growth <- correct_growth %>% pivot_wider(names_from = "treatment", value
 correct_growth$growth_corr <- correct_growth$not_grazed- correct_growth$grazed
 trial1_data <- left_join(trial1_data, correct_growth, by = "sample_ID")
 trial1_data$growth_corr[trial1_data$treatment =="not_grazed"]=NA
-
-# Grazed and baseline separation #### 
-trial1_data_ex_c <-trial1_data[trial1_data$treatment == 'grazed', ]   #Gazed, without baseline
-trial1_data_c <-trial1_data[trial1_data$treatment == 'not_grazed', ]  #Baseline
 
 
 ###################### Trial 2: preference #####################################
