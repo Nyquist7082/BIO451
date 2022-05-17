a7c1a733a162c1b6db11fb362b5ca3f0caf5674f
hartiem <emma.j.hartikainen@gmail.com>
2022-05-17 12:58:51 +0200

    plot wet weight diff % for seltered and exposed choice no_choise

--- BIO451_5_combined_grazing.R
+++ BIO451_5_combined_grazing.R
@@ -12,3 +12,18 @@ library(corrplot)
 library(tidyverse)     #Data manipulation
 library(rstatix)       #Pipe-friendly R functions for easy statistical analyses
 library(googlesheets4) #Read google sheets data into R 
+
+#Plot wet weight diff exposed and sheltered choice no choice####
+p_comb_shel<- ggboxplot(combined_data_sheltered, x = "experiment", y = "ww_perc",
+                        color = "experiment",
+                        add = "jitter", shape = "ecotype", width= 0.15)+
+  theme_classic()
+
+p_comb_shel+ theme(legend.title = element_blank())
+
+p_comb_expo<- ggboxplot(combined_data_exposed, x = "experiment", y = "ww_perc",
+                        color = "experiment",
+                        add = "jitter", shape = "ecotype", width= 0.15)+
+  theme_classic()
+
+p_comb_expo+ theme(legend.title = element_blank())
